5678b839b347c8ade73d1d8728f9c0c66c5b9dfa
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2024-02-10 15:17:56 +0100

    to test api request

---
+++ print_calculation.py
@@ -0,0 +1,29 @@
+
+
+
+
+
+import requests
+
+def make_request_to_url(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            if response.content.decode('utf-8').strip() == "1":
+                return 1
+            else:
+                print("response.content.decode", response.content.decode('utf-8'))
+
+                return 0
+        else:
+            print("Error: Unable to fetch data from the URL", response.content.decode('utf-8'))
+            return 0
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+        return 0
+
+# Example usage:
+url = 'https://www.random.org/integers/?num=1&min=1&max=6&col=5&base=10&format=plain&rnd=new'
+result = make_request_to_url(url)
+print(result)
+
